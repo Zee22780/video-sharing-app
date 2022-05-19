e20f2796767e4520b963467d4cb2eaf51a6a0b87
zuri227 <zuri22780@gmail.com>
2022-05-19 19:27:30 -0400

    Add grid layout to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Grid from '@mui/material/Grid'
 function App() {
   return (
     <div className="App">
-      <h1>Video Sharing Application</h1>
+      <Grid style={{justifyContent: "center"}} container spacing={10}>
+        <Grid item xs={11}>
+          <Grid container spacing={10}>
+            <Grid>
+              {/* {SearchBar} */}
+            </Grid>
+            <Grid>
+              {/* {VideoDetail} */}
+            </Grid>
+            <Grid>
+              {/* {VideoList} */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
