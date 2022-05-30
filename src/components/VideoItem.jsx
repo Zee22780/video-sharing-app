875c04e0f5468263d38cbcab3781da5dfd01145e
zuri227 <zuri22780@gmail.com>
2022-05-30 19:48:55 -0400

    Add border to video items

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -3,7 +3,8 @@ import { Grid, Paper, Typography } from "@mui/material"
 const VideoItem = ({ video, onVideoSelect }) => {
   return ( 
     <Grid item xs={12}>
-      <Paper style={{ display: "flex", alignItems: "center", cursor: "pointer" }} onClick={() => onVideoSelect(video)} >
+      <Paper style={{ display: "flex", alignItems: "center", cursor: "pointer", border:"1px solid black"}} 
+      onClick={() => onVideoSelect(video)} >
         <img style={{ marginRight: "20px" }} alt="thumbnail" src={video.snippet.thumbnails.medium.url} />
         <Typography variant="subtitle1">
           <b>{video.snippet.title}</b>
